a8a9e07fd1656d6f77d084a6f9832ef227c75eef
nanda <parmi24@gmail.com>
2021-02-09 05:44:56 -0500

    se

--- app/src/main/java/com/example/alpha/activities/video/PlayVideoActivity.kt
+++ app/src/main/java/com/example/alpha/activities/video/PlayVideoActivity.kt
@@ -3,10 +3,13 @@ package com.example.alpha.activities.video
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alpha.R
+import kotlinx.android.synthetic.main.activity_record_audio.view.*
 
 class PlayVideoActivity : AppCompatActivity() {
 
@@ -14,6 +17,9 @@ class PlayVideoActivity : AppCompatActivity() {
     private lateinit var tv_judul: TextView
     private lateinit var tv_view: TextView
     private lateinit var tv_ago: TextView
+    private lateinit var btn_play: ImageView
+    private lateinit var btn_pause: ImageView
+    private lateinit var btn_stop: ImageView
 
     var judul: String?=""
     var view: String?=""
@@ -29,10 +35,17 @@ class PlayVideoActivity : AppCompatActivity() {
     }
 
     fun findId(){
+
         vv=findViewById(R.id.vv)
+
         tv_judul=findViewById(R.id.tv_judul)
         tv_view=findViewById(R.id.tv_view)
         tv_ago=findViewById(R.id.tv_ago)
+        btn_pause=findViewById(R.id.btn_pause)
+        btn_play=findViewById(R.id.btn_play)
+        btn_stop=findViewById(R.id.btn_stop)
+
+
 
         var intent=intent
         judul=intent.getStringExtra("judul")
@@ -47,6 +60,18 @@ class PlayVideoActivity : AppCompatActivity() {
         val uri: Uri = Uri.parse("https://www.sample-videos.com/video123/mp4/480/big_buck_bunny_480p_30mb.mp4")
         vv.setVideoURI(uri)
         vv.start()
+
+        btn_pause.setOnClickListener(View.OnClickListener {
+            vv.pause()
+        })
+
+        btn_stop.setOnClickListener(View.OnClickListener {
+            vv.stop
+        })
+
+        btn_play.setOnClickListener(View.OnClickListener {
+            vv.play
+        })
 
     }
 
